4984661198fa020d7d3f7d314f6147c246f5a584
DESKTOP-M9JJK92\admin <l>
2024-10-10 23:14:52 +0300

    zoneController оптимизирован

--- models/zoneController.go
+++ models/zoneController.go
@@ -6,37 +6,41 @@ import (
 	"sync"
 )
 
+type ResponseData struct {
+	Zone    []Zone `json:"zone"`
+	Map     []Map  `json:"map"`
+	MapZone []Zone `json:"mapZone"`
+}
+
 func GetMapPosition(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
-	wg.Add(3) // Увеличиваем счетчик на количество горутин, которые нужно ожидать
+	wg.Add(3)
+
 	// Разбор параметров запроса
 	r.ParseForm()
 	clubId := r.Form.Get("club_id")
 
-	// Создание канала для передачи данных о зонах
-	var zoneChan = make(chan []Zone, 1)
-	var zone2Chan = make(chan []Zone, 1)
-	var mapChan = make(chan []Map, 1)
+	// Структура для сбора данных
+	var data ResponseData
 
-	// Запуск горутины для получения
-	go getZone(clubId, zone2Chan, &wg)
-	go getMap(clubId, mapChan, &wg)
-	go getMapZone(clubId, zoneChan, &wg)
+	// Запуск горутин
+	go func() {
+		defer wg.Done()
+		data.Zone = getZone(clubId)
+	}()
+	go func() {
+		defer wg.Done()
+		data.Map = getMap(clubId)
+	}()
+	go func() {
+		defer wg.Done()
+		data.MapZone = getMapZone(clubId)
+	}()
 
 	wg.Wait()
-	// Получение данных из каналов
-	zones := <-zoneChan
-	zone2 := <-zone2Chan
-	map1 := <-mapChan
-
-	outData := map[string]interface{}{
-		"zone":    zone2,
-		"map":     map1,
-		"mapZone": zones,
-	}
 
 	// Преобразование данных в формат JSON
-	zoneData, err := json.Marshal(outData)
+	zoneData, err := json.Marshal(data)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -47,9 +51,7 @@ func GetMapPosition(w http.ResponseWriter, r *http.Request) {
 	w.Write(zoneData)
 }
 
-func getMapZone(clubId string, zoneChan chan []Zone, wg *sync.WaitGroup) {
-	defer wg.Done()
-	// Получение данных о зонах из базы данных
+func getMapZone(clubId string) []Zone {
 	var zones []Zone
 	err := Database.Table("zone").
 		Select("zone.*").
@@ -59,39 +61,25 @@ func getMapZone(clubId string, zoneChan chan []Zone, wg *sync.WaitGroup) {
 		Find(&zones).Error
 
 	if err != nil {
-		// Обработка ошибки или отправка ошибки в канал
-		zoneChan <- nil
-		return
+		return nil // Или обработка ошибки
 	}
-
-	// Отправка данных о зонах в канал
-	zoneChan <- zones
+	return zones
 }
 
-func getZone(clubId string, zone2Chan chan []Zone, wg *sync.WaitGroup) {
-	defer wg.Done()
+func getZone(clubId string) []Zone {
 	var zone []Zone
 	err := Database.Where("club_id = ?", clubId).Find(&zone).Error
 	if err != nil {
-		// Обработка ошибки или отправка ошибки в канал
-		zone2Chan <- nil
-		return
+		return nil // Или обработка ошибки
 	}
-
-	// Отправка данных о зонах в канал
-	zone2Chan <- zone
+	return zone
 }
 
-func getMap(clubId string, map1 chan []Map, wg *sync.WaitGroup) {
-	defer wg.Done()
+func getMap(clubId string) []Map {
 	var maps []Map
 	err := Database.Where("club_id = ?", clubId).Find(&maps).Error
 	if err != nil {
-		// Обработка ошибки или отправка ошибки в канал
-		map1 <- nil
-		return
+		return nil // Или обработка ошибки
 	}
-
-	// Отправка данных о зонах в канал
-	map1 <- maps
+	return maps
 }
